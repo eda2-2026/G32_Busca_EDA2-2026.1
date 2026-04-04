d429a7f3d31761b21d2aad26c63ffbb036786a8f
Raphides <raphaelmendesdasilva@gmail.com>
2026-04-04 12:48:53 -0300

    Troca de dict para list na estrutura da tabela de índices. Busca Sequencial Completa nos índices da tabela de índices.

--- main.py
+++ main.py
@@ -8,24 +8,32 @@ with open("./qa.csv", "rt") as qa_csv:
 # Building index table 
 qa_categories = set()
 qa_categories.add(qa_list[1][0])
-qa_index_table = {qa_list[1][0]: 1}
+qa_index_table = [[qa_list[1][0], 1]]
 
 for qa_i in range(2, len(qa_list)):
     set_size = len(qa_categories)
     qa_categories.add(qa_list[qa_i][0])
     set_size -= len(qa_categories)
     if (set_size == -1):
-        qa_index_table[qa_list[qa_i][0]] = qa_i
+        qa_index_table.append([qa_list[qa_i][0], qa_i])
 
 print(qa_index_table)
 
+# Sequencial Search in Index Table
+def qa_index_table_sequencial_search(category):
+    for qa_index_table_item in qa_index_table:
+        if qa_index_table_item[0] == category:
+            return qa_index_table_item
+
+# Filter questions by using Index Table.
 def qa_filter(category):
     result = []
-    for i in range(qa_index_table[category], len(qa_list)):
+    for i in range(qa_index_table_sequencial_search(category)[1], len(qa_list)):
         if qa_list[i][0] != category:
             break
         result.append(qa_list[i])
     return result
+
 print(qa_filter("Oauth2"))
 print(qa_filter("Aprendizado de Máquina"))
 print(qa_filter("Arquitetura de Inteligência de Negócio"))
