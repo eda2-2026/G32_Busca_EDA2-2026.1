0593e5b00527dc5d864e42eadb507060cf841023
Raphides <raphaelmendesdasilva@gmail.com>
2026-04-04 11:10:16 -0300

    Tabela de Index Construída

--- main.py
+++ main.py
@@ -1,10 +1,23 @@
 import csv
 
-
 qa_list = None
 with open("./qa.csv", "rt") as qa_csv:
-        qa_list = list(csv.reader(qa_csv, dialect="excel"))
-        print("csv carregado")
+    qa_list = list(csv.reader(qa_csv, dialect="excel"))
+    print("csv carregado")
+
+# Building index table 
+qa_categories = set()
+qa_categories.add(qa_list[1][0])
+qa_index_table = {qa_list[1][0]: 1}
+
+for qa_i in range(2, len(qa_list)):
+    set_size = len(qa_categories)
+    qa_categories.add(qa_list[qa_i][0])
+    set_size -= len(qa_categories)
+    if (set_size == -1):
+        qa_index_table[qa_list[qa_i][0]] = qa_i
+
+print(qa_index_table)
 
 def qa_filter(category):
     # Search code here
